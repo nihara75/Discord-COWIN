3af93ba18e6d4ec42ebf380e09e12467dc07f0d6
nihara75 <niharar25@gmail.com>
2021-05-26 12:02:31 +0530

    subscription

--- bot.js
+++ bot.js
@@ -107,6 +107,15 @@ client.on('ready',async ()=>{
 	
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 	
 client.on('message',async (msg)=>{
 
